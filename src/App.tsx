b79553d2ca9c35412781cad725fadf83c7f091f3
BabcB <85190284+BabcB@users.noreply.github.com>
2024-10-15 15:13:30 +0000

    startTransition changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import './App.css';
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+
   const [count, setCount] = useState(0);
+  const [bool, setbool] = useState(false);
+
+  const handleClick = () => {
+    setCount((count) => count + 1);
+    console.log('out');
+
+    startTransition(() => {
+      console.log('in');
+      setbool((prev) => !prev);
+    });
+  };
+
+  console.log({ count, bool, isPending });
 
   return (
     <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+      <button onClick={handleClick}>
+        count is {count} {bool}
       </button>
+      {isPending ? 'Loading...' : ''}
     </div>
   );
 }
